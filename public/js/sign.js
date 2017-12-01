7721d8dd679932770354720e888539953dc20b36
lancelot-song <896231585@qq.com>
2017-12-01 17:27:59 +0800

    首页UI修改，加入首屏滚动区域

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,39 +1,17 @@
-$(function(){
-
-	//用户注册逻辑
-	$("#signup,#goSignup").submit(function(){
-		var $this = $(this);
-		$.ajax({
-			type : "POST",
-			url  : $this.attr("action"),
-			data : $this.serialize()
-		})
-		.done(function(data){
-			if(data.status){
-				window.location.href = data.url;
-			}else{
-				console.log(data.info)
-			}
-		})
-		return false;
-	});
-
-	//用户登录逻辑
-	$("#signin,#goSignin").submit(function(){
-		var $this = $(this);
-		$.ajax({
-			type : "POST",
-			url  : $this.attr("action"),
-			data : $this.serialize()
-		})
-		.done(function(data){
-			if(data.status){
-				window.location.href = data.url;
-			}else{
-				console.log(data.info)
-			}
-		})
-		return false;
-	});
-
-})
+/* 登录注册逻辑 */
+$("#signin,#goSignin,#signup,#goSignup").submit(function(){
+	var $this = $(this);
+	$.ajax({
+		type : "POST",
+		url  : $this.attr("action"),
+		data : $this.serialize()
+	})
+	.done(function(data){
+		if(data.status){
+			window.location.href = data.url;
+		}else{
+			console.log(data.info)
+		}
+	})
+	return false;
+});
